b526bdf646488367b6f818816b3fde865a4531de
Marjolin Jahja <108075423+Marjolin1997@users.noreply.github.com>
2026-09-09 15:45:40 +0200

    feat(rbac): enable roles and permission-aware identity

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, HasRoles, Notifiable;
+
+    protected string $guard_name = 'web';
 
     protected $fillable = ['name', 'first_name', 'last_name', 'username', 'job_title', 'bio', 'avatar_disk', 'avatar_path', 'email', 'password'];
     protected $hidden = ['password', 'remember_token', 'avatar_disk', 'avatar_path'];
@@ -18,6 +21,15 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return ['email_verified_at' => 'datetime', 'password' => 'hashed'];
+    }
+
+    public function identityPayload(): array
+    {
+        return $this->only(['id', 'name', 'first_name', 'last_name', 'username', 'job_title', 'bio', 'email']) + [
+            'has_avatar' => (bool) ($this->avatar_disk && $this->avatar_path),
+            'roles' => $this->getRoleNames()->values()->all(),
+            'permissions' => $this->getAllPermissions()->pluck('name')->sort()->values()->all(),
+        ];
     }
 
     public function categories(): HasMany { return $this->hasMany(Category::class); }
