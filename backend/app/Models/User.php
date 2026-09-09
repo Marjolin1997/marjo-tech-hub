77f04678db624272bbce22909a16c78e0eb8aac3
Marjolin Jahja <108075423+Marjolin1997@users.noreply.github.com>
2026-09-09 11:25:55 +0200

    feat(profile): expose editable profile fields on user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -12,8 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $fillable = ['name', 'email', 'password'];
-    protected $hidden = ['password', 'remember_token'];
+    protected $fillable = ['name', 'first_name', 'last_name', 'username', 'job_title', 'bio', 'avatar_disk', 'avatar_path', 'email', 'password'];
+    protected $hidden = ['password', 'remember_token', 'avatar_disk', 'avatar_path'];
 
     protected function casts(): array
     {
